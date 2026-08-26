abf456c2de2c62264a4a57f47d6da98c8cb80232
Isadora Ferreira Alvarenga <1558374@sga.pucminas.br>
2026-08-26 12:05:30 -0300

    feat: Adiciona lógica inicial, primeiro end point e base url da api

--- ClimaAPI/src/main/java/com/example/ClimaAPI/controller/Controller.java
+++ ClimaAPI/src/main/java/com/example/ClimaAPI/controller/Controller.java
@@ -1,5 +1,23 @@
 package com.example.ClimaAPI.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ClimaAPI.service.Service;
+
+@RestController
 public class Controller {
+
+    Service service = new Service();
+
+    @GetMapping("/clima")
+    public String consultarClima(){
+        return service.consultarClima();
+    }
+    @GetMapping("/clima/{cidade}") // https://localhost:8080/climaBH
+    public String consultarClimaCidade(@PathVariable String cidade){
+        return service.consultarClimaCidade(cidade);
+    }
     
 }
--- ClimaAPI/src/main/java/com/example/ClimaAPI/service/Service.java
+++ ClimaAPI/src/main/java/com/example/ClimaAPI/service/Service.java
@@ -1,5 +1,7 @@
 package com.example.ClimaAPI.service;
 
 public class Service {
+
+    private static final String BASE_URL = "https://api.open-meteo.com/v1";
     
 }
